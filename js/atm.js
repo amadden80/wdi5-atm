e5c062b6be8825661902ef4cb7e8186759da0769
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-06-18 15:28:58 -0400

    cleaning js syntax

--- js/atm.js
+++ js/atm.js
@@ -2,21 +2,11 @@ var checking_balance = 1000.00;
 var savings_balance = 1000.00;
 
 function deposit_into_checking(){
-
   var amount_input = parseFloat($('#checking input[name=amount]').val());
   checking_balance += amount_input;
   $('#checking span.balance').text(checking_balance);
-
-
-  // $('#checking ')
-
-  console.log(amount_input);
 }
 
-$(document).ready(
-  function(){
-
-    $('#checking .deposit').click(deposit_into_checking);
-
-  }
-);
+$(document).ready(function(){
+  $('#checking .deposit').click(deposit_into_checking);
+});
